76f3db06afa4874e345038ff670d5e56b9d8d3c1
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2024-11-07 11:25:25 +0700

    change

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.menu-item').forEach(item => {
+    item.addEventListener('click', function() {
+      document.querySelectorAll('.menu-item').forEach(menu => menu.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+  
+    // Mengecek arah scroll
+    if (window.scrollY > lastScrollY) {
+      // Jika scroll ke bawah, sembunyikan header
+      header.classList.add('hide');
+    } else {
+      // Jika scroll ke atas, tampilkan header
+      header.classList.remove('hide');
+    }
+    
+    lastScrollY = window.scrollY; // Update posisi scroll terakhir
+  });
